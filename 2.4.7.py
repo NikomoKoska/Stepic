8c194f11dac65bd5827e6362596f95a7723bab99
NikomoKoska <zanddanglokta13@gmail.com>
2020-05-20 22:16:45 +0300

    начало мысли

--- 2.4.7.py
+++ 2.4.7.py
@@ -8,19 +8,27 @@
 #         result_string += c
 #     prev_symbol = c
 input_string = input('Enter string ')
+str_len = len(input_string)
 result_string = ''
 
 start_num = 0
-end_num = 0
 num = 1
 
-for i in range(len(input_string)):
-    if str[start_num] == str[end_num]:
-        result_string += str[start_num] + str(num)
-        start_num = i
-    else:
-        num = 1
-    end_num = i
+for i in range(str_len+1):
+    for j in input_string[i+1:]:
+        
+
+    # end_num = start_num + 1
+    # if start_num + 1 >= str_len:
+    #     end_num = start_num
+
+    # if input_string[start_num] == input_string[end_num:end_num]:
+    #     num += 1
+    # else:
+    #     result_string += input_string[start_num] + str(num)
+    #     num = 1
+    # start_num = i
+
 
 print(result_string)
 
