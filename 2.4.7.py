9f635fd7d0c749e48578534f7db4959c694189c2
NikomoKoska <zanddanglokta13@gmail.com>
2020-05-20 22:55:25 +0300

    работает

--- 2.4.7.py
+++ 2.4.7.py
@@ -1,36 +1,18 @@
-# num = 1
-# prev_symbol = ''
-
-# for c in str:
-#     if c == prev_symbol:
-#         num += 1
-#     else
-#         result_string += c
-#     prev_symbol = c
 input_string = input('Enter string ')
 str_len = len(input_string)
 result_string = ''
-
-start_num = 0
 num = 1
 
-for i in range(str_len+1):
-    for j in input_string[i+1:]:
-        
+for i in range(str_len):
+    symbol = input_string[i]
+    if i == str_len - 1:
+        result_string += symbol + str(num)
+        break
 
-    # end_num = start_num + 1
-    # if start_num + 1 >= str_len:
-    #     end_num = start_num
-
-    # if input_string[start_num] == input_string[end_num:end_num]:
-    #     num += 1
-    # else:
-    #     result_string += input_string[start_num] + str(num)
-    #     num = 1
-    # start_num = i
-
+    if symbol == input_string[i + 1]:
+        num += 1
+    else:
+        result_string += symbol + str(num)
+        num = 1
 
 print(result_string)
-
-# str = 'a'
-# print(str[0:])
