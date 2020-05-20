36ee9ff962a3e8da76abb7b4ef03bde5929fa8dd
NikomoKoska <zanddanglokta13@gmail.com>
2020-05-20 23:04:07 +0300

    добавил решение препода

--- 2.4.7.py
+++ 2.4.7.py
@@ -16,3 +16,15 @@ for i in range(str_len):
         num = 1
 
 print(result_string)
+
+#  решение на курсе
+
+# genome = input()+' '
+# s = 0
+# n=genome[0]
+# for i in genome:
+#     if n!=i:
+#         print(n + str(s), end = '')
+#         s=0
+#         n=i
+#     s+=1
