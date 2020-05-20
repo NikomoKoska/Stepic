2c7d3a2a9e37dcf25757ee76b33e9dab15f068cb
NikomoKoska <zanddanglokta13@gmail.com>
2020-05-20 21:38:07 +0300

    111

--- 2.4.7.py
+++ 2.4.7.py
@@ -1,6 +1,28 @@
-s = input()
-result_string = ''
-num = 0
+# num = 1
+# prev_symbol = ''
 
-for target_list in expression_list:
-    pass 
+# for c in str:
+#     if c == prev_symbol:
+#         num += 1
+#     else
+#         result_string += c
+#     prev_symbol = c
+input_string = input('Enter string ')
+result_string = ''
+
+start_num = 0
+end_num = 0
+num = 1
+
+for i in range(len(input_string)):
+    if str[start_num] == str[end_num]:
+        result_string += str[start_num] + str(num)
+        start_num = i
+    else:
+        num = 1
+    end_num = i
+
+print(result_string)
+
+# str = 'a'
+# print(str[0:])
