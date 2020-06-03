f7e0565c920ee2ca2714b957267465be125f6687
NikomoKoska <zanddanglokta13@gmail.com>
2020-06-03 19:22:22 +0300

    start namespaces

---
+++ Step 2/1.4.10 NameSpaces.py
@@ -0,0 +1,6 @@
+# global_namespaces = {'global': {'parent': None, 'var': []}}
+# def create(namespace, parent):
+#     if parent not in global_namespaces:
+#         global_namespaces[namespace] = {'parent': 'global', 'var': []}
+#     else:
+#
