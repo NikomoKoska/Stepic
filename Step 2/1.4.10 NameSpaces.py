3aab8633efe1a991357de0a64aebc009e8f9d19b
NikomoKoska <zanddanglokta13@gmail.com>
2020-06-07 21:13:36 +0300

    done 1.4

--- Step 2/1.4.10 NameSpaces.py
+++ Step 2/1.4.10 NameSpaces.py
@@ -1,6 +1,24 @@
-# global_namespaces = {'global': {'parent': None, 'var': []}}
-# def create(namespace, parent):
-#     if parent not in global_namespaces:
-#         global_namespaces[namespace] = {'parent': 'global', 'var': []}
-#     else:
-#
+global_namespaces = {'global': {'parent': None, 'var': []}}
+def create(namespace, parent):
+    global_namespaces[namespace] = {'parent': parent, 'var': []}
+
+def add(namespace, var):
+    global_namespaces[namespace]['var'].append(var)
+
+def get(namespace, var):
+    if namespace == 'global' and var not in global_namespaces[namespace]['var']:
+        return None
+    if var in global_namespaces[namespace]['var']:
+        return namespace
+    else:
+        return get(global_namespaces[namespace]['parent'], var)
+
+num = int(input())
+for i in range(num):
+    cmd, namesp, arg = input().split()
+    if cmd == 'add':
+        add(namesp, arg)
+    elif cmd == 'create':
+        create(namesp, arg)
+    elif cmd == 'get':
+        print(get(namesp, arg))
